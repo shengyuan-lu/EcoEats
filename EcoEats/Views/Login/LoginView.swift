2ee052606a15d6143620d024b1dab176f71c6349
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2021-05-08 08:15:23 -0700

    Branch out login

--- EcoEats/Views/Login/LoginView.swift
+++ EcoEats/Views/Login/LoginView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct LoginView: View {
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+    
 }
 
 struct LoginView_Previews: PreviewProvider {
