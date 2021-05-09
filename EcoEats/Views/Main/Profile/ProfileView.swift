22c9c96be3dd8571a5db880733c80d307ffcb2d2
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2021-05-09 08:26:31 -0700

    Final Commit

--- EcoEats/Views/Main/Profile/ProfileView.swift
+++ EcoEats/Views/Main/Profile/ProfileView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ProfileView: View {
     
@@ -15,10 +16,39 @@ struct ProfileView: View {
         
         NavigationView {
             
-            VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            ScrollView {
+                VStack {
+                    VStack(spacing: 10) {
+                        
+                        Image("shengyuan")
+                            .resizable()
+                            .frame(width: 120, height: 120)
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.init(hex: "54925A"), lineWidth: 5))
+                        
+                        
+                        Text("Shengyuan")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.init(hex: "54925A"))
+                            .lineLimit(2)
+                        
+                        
+                        LineView(data: [8,23,54,32,12,37,7,23,43], title: "My Carbon Footprint", legend: "Weekly / (CO2e)", style: ChartStyle(backgroundColor: Color.white, accentColor: Color.init(hex: "54925A"), gradientColor: GradientColor(start: Color.init(hex: "54925A"), end: Color.init(hex: "54925A")), textColor: Color.primary, legendTextColor: Color.init(hex: "54925A"), dropShadowColor: Color.black), valueSpecifier:  "%.0f")
+                            .padding(16)
+                        
+                        
+                    }
+                    
+
             }
-            .navigationBarTitle(Text("Welcome, Shengyuan"))
+            
+
+                
+            }
+            .navigationBarTitle(Text("Profile"))
             .navigationBarTitleDisplayMode(.large)
             .navigationBarItems(trailing:
                                     
